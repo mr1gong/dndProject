a2faf7758439c4dd5220d17baf65b1da5963a2eb
Khaloth <kostlivec4@gmail.com>
2019-05-07 18:10:01 +0200

    The settings menu has been given a now functional volume slider + all the future AI elements. The menu itself is now closable.

--- GitProject/Assets/Scripts/MainMenu.cs
+++ GitProject/Assets/Scripts/MainMenu.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer MainMenuMixer;
     public GameObject Buttons;
+    public GameObject SettingsMenu;
     // Start is called before the first frame update
-    void Start()
+    
+    //Adjusts the game volume in accordance with the main menu settings volume slider
+    public void SetVolume(float inputVolume)
     {
+        MainMenuMixer.SetFloat("MasterVolume", inputVolume);
 
     }
 
-    public void Disable()
+    public void DisableButtons()
     {
         Buttons.GetComponent<GameObject>();
         Buttons.SetActive(false);
     }
 
-    public void Enable()
+    public void EnableButtons()
     {
         Buttons.GetComponent<GameObject>();
         Buttons.SetActive(true);
+    }
+
+    public void EnableSettings()
+    {
+        SettingsMenu.GetComponent<GameObject>();
+        SettingsMenu.SetActive(true);
+    }
+
+    public void DisableSettings()
+    {
+        SettingsMenu.GetComponent<GameObject>();
+        SettingsMenu.SetActive(false);
     }
 }
