3e31291819c4b1fd8b95f3cb3f7f59e3353f60a1
Khaloth <kostlivec4@gmail.com>
2019-05-08 13:54:26 +0200

    Finalisation of the settings menu + implementation of a custom cursor

--- GitProject/Assets/Scripts/MainMenu.cs
+++ GitProject/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
+﻿//Author: Novak
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer MainMenuMixer;
     public GameObject Buttons;
     public GameObject SettingsMenu;
-    // Start is called before the first frame update
-    
-    //Adjusts the game volume in accordance with the main menu settings volume slider
+    public Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        int screenResolutionIdex = 0;
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(toList(resolutions));
+
+        resolutionDropdown.value = screenResolutionIdex;
+        resolutionDropdown.RefreshShownValue();
+
+        List<string> toList(Resolution[] input)
+        {
+            List<string> output = new List<string>();
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string item = resolutions[i].width + " by " + resolutions[i].height;
+                output.Add(item);
+
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    screenResolutionIdex = i;
+                }
+            }
+            return output;
+        }
+    }
+
     public void SetVolume(float inputVolume)
     {
         MainMenuMixer.SetFloat("MasterVolume", inputVolume);
@@ -39,5 +70,21 @@ public class MainMenu : MonoBehaviour
     {
         SettingsMenu.GetComponent<GameObject>();
         SettingsMenu.SetActive(false);
+    }
+
+    public void SetGraphicsQuality (int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void TriggerFullscreenMode(bool input)
+    {
+        Screen.fullScreen = input;
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
