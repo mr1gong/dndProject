c35a0061e7743068b5d84451b09461b6b035c973
Khaloth <kostlivec4@gmail.com>
2019-05-26 14:01:09 +0200

    Finalisation of the Journal (without any acitve quests) and initial setup of the Inventory + Scripts for both

--- GitProject/Assets/Scripts/Item.cs
+++ GitProject/Assets/Scripts/Item.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item
 {
     public string Name;
     public string Description;
+    public Image ItemImage;
 }
--- GitProject/Assets/Scripts/UI.cs
+++ GitProject/Assets/Scripts/UI.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
+    public GameObject Inventory;
+
+    public GameObject QuestSlot0;
+    public GameObject QuestSlot1;
+    public GameObject QuestSlot2;
+
+    public GameObject WeaponSlot0;
+    public GameObject WeaponSlot1;
+    public GameObject WeaponSlot2;
+
     public GameObject Journal;
+    public Text MainObjective;
+    public Text CurrentObjective;
+
+    private List<GameObject> SlotList = new List<GameObject>();
     private List<GameObject> UiList = new List<GameObject>();
     private Dictionary<int, GameObject> Windows = new Dictionary<int, GameObject>();
 
@@ -20,9 +36,35 @@ public class UI : MonoBehaviour
         CheckInput();
     }
 
+    public void ChangeMainObjective(string input)
+    {
+        ClearText(MainObjective);
+        MainObjective.text = input;
+    }
+
+    public void ChangeSecondaryObjective(string input)
+    {
+        ClearText(CurrentObjective);
+        CurrentObjective.text = input;
+    }
+
+    public void ClearText(Text input)
+    {
+        input = null;
+    }
+
     private void Initalise()
     {
+        SlotList.Add(QuestSlot0);
+        SlotList.Add(QuestSlot1);
+        SlotList.Add(QuestSlot2);
+
+        SlotList.Add(WeaponSlot0);
+        SlotList.Add(WeaponSlot1);
+        SlotList.Add(WeaponSlot2);
+
         UiList.Add(Journal);
+        UiList.Add(Inventory);
 
         for (int index = 0; index < UiList.Count; index++)
         {
@@ -44,7 +86,10 @@ public class UI : MonoBehaviour
         {
             EnableDisableWindow(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            EnableDisableWindow(1);
+        }
     }
-
-
 }
