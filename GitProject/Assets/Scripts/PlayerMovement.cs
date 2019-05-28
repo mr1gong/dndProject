e054759114816299383e93b6267aa17deebb7a50
MrRadpower <mr.gong78@gmail.com>
2019-05-28 23:38:51 +0200

    Navmesh + BlendTree

---
+++ GitProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public LayerMask Clickable;
+    private NavMeshAgent navAgent;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 50 , Clickable))
+            {
+                navAgent.SetDestination(hit.point);
+            }
+        }
+        animator.SetFloat("Speed", Mathf.Abs(navAgent.velocity.x));
+    }
+}
