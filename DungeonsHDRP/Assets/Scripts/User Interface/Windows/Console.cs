c5dd9e670c725502d0be5dff50f6b1b2e00559a8
Khaloth <kostlivec4@gmail.com>
2020-04-12 14:25:58 +0200

    Console-Window Transition

---
+++ DungeonsHDRP/Assets/Scripts/User Interface/Windows/Console.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : UIElement
+{
+    #region Fields
+    private static Console _ConsoleInstance;
+    public RectTransform Prime;
+    private bool _TransitionOut = false;
+    private bool _TransitionIn = false;
+
+    public GameObject ConsoleWindow;
+    #endregion
+
+    void Start()
+    {
+        _TransitionOut = false;
+        _TransitionIn = false;
+    }
+
+    void Update()
+    {
+        if (_TransitionIn)
+        {
+            TransitionIn();
+        }
+
+        if (_TransitionOut)
+        {
+            TransitionOut();
+        }
+    }
+
+    public static Console GetInstance()
+    {
+        if (_ConsoleInstance == null)
+        {
+            _ConsoleInstance = FindObjectOfType<Console>();
+        }
+        return _ConsoleInstance;
+    }
+
+    public void EnableConsole()
+    {
+        ConsoleWindow.SetActive(true);
+    }
+
+    public void DisableConsole()
+    {
+        ConsoleWindow.SetActive(false);
+    }
+
+    public void StartTransitionIn()
+    {
+        _TransitionIn = true;
+        Debug.Log($"Transitioning: {ConsoleWindow.transform.position.y}");
+        Debug.Log($"Transitioning: {ConsoleWindow.transform.position.x}");
+        Debug.Log("Transition Started");
+    }
+
+    public void StartTransitionOut()
+    {
+        _TransitionOut = true;
+    }
+
+    public void TransitionIn()
+    {
+        _TransitionOut = false;
+
+        if (ConsoleWindow.transform.position.x >= 750)
+        {
+            _TransitionIn = false;
+            return;
+        }
+
+        //ConsoleWindow.transform.position.Set(ConsoleWindow.transform.position.x + 1, ConsoleWindow.transform.position.y, ConsoleWindow.transform.position.z);
+        Prime.transform.position = new Vector2(ConsoleWindow.transform.position.x + 1, 345.5f);
+    }
+
+    public void TransitionOut()
+    {
+        _TransitionIn = false;
+
+        if (ConsoleWindow.transform.position.x <= 545.7283f)
+        {
+            _TransitionIn = false;
+            return;
+        }
+
+        Prime.transform.position = new Vector2(ConsoleWindow.transform.position.x - 1, 345.5f);
+    }
+}
